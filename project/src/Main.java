cfb706728ade49c83699b4314ae30aa36272e4ef
Leon Dierkes <leon.dierkes@tec2date.de>
2022-09-29 13:57:50 +0200

    Updated Button Alignment.

--- project/src/Main.java
+++ project/src/Main.java
@@ -25,5 +25,27 @@ public class Main {
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridBagLayout());
+
+        JButton buttonLeft = new JButton("Links");
+        JButton buttonCenter = new JButton("Mitte");
+        JButton buttonRight = new JButton("Rechts");
+
+        GridBagConstraints frameGridConstraints = new GridBagConstraints();
+
+        frameGridConstraints.weightx = 0.5;
+        frameGridConstraints.gridx = 0;
+        frameGridConstraints.gridy = 0;
+        frame.add(buttonLeft, frameGridConstraints);
+
+        frameGridConstraints.weightx = 0.5;
+        frameGridConstraints.gridx = 1;
+        frameGridConstraints.gridy = 0;
+        frame.add(buttonCenter, frameGridConstraints);
+
+        frameGridConstraints.weightx = 0.5;
+        frameGridConstraints.gridx = 2;
+        frameGridConstraints.gridy = 0;
+        frame.add(buttonRight, frameGridConstraints);
     }
 }
