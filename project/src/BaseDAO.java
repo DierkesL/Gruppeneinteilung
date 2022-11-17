86e7054ac7f2c1a3bb32a4f5538db2b6b5a31740
Leon Dierkes <leon.dierkes@tec2date.de>
2022-11-17 22:08:01 +0100

    Implemented basic configuration system

--- project/src/BaseDAO.java
+++ project/src/BaseDAO.java
@@ -1,20 +1,51 @@
+import org.json.JSONObject;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
+import java.io.*;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BaseDAO {
-    public DataSource createDataSource() {
+
+    public JSONObject readConfigJSON() {
+        try {
+            File configFile = new File(getClass().getResource("config.json").toURI());
+            BufferedReader buffer = new BufferedReader(new FileReader(configFile));
+            String configFileContent = "";
+            String bufferString = "";
+
+            while((bufferString = buffer.readLine()) != null) {
+                if(configFileContent != "") {
+                    configFileContent += "\n" + bufferString;
+                }else{
+                    configFileContent += bufferString;
+                }
+            }
+
+            return new JSONObject(configFileContent);
+        } catch(IOException | URISyntaxException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return null;
+    }
+
+    public JSONObject getConfigDatabaseCredentials(JSONObject configJSON) {
+        return new JSONObject(configJSON.get("database").toString());
+    }
+
+    public DataSource createDataSource(JSONObject configDatabase) {
         PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setUrl("jdbc:postgresql://db-grpeinteilung-3751.7tc.cockroachlabs.cloud/");
-        ds.setPortNumber(26257);
-        ds.setDatabaseName("db-grpeinteilung-3751.Gruppeneinteilung");
-        ds.setUser("testuser");
-        ds.setPassword("_jnQ_7PPPz7j_LJ5PSCbCg");
-        ds.setSslMode("require");
+        ds.setUrl(configDatabase.get("db_url").toString());
+        ds.setPortNumber((int)configDatabase.get("db_port"));
+        ds.setDatabaseName(configDatabase.get("db_name").toString());
+        ds.setUser(configDatabase.get("db_user").toString());
+        ds.setPassword(configDatabase.get("db_password").toString());
+        ds.setSslMode(configDatabase.get("db_sslmode").toString());
 
         try{
             if(ds.getConnection() != null) {
@@ -31,7 +62,7 @@ public class BaseDAO {
     }
 
     public void testQuery() {
-        DataSource ds = createDataSource();
+        DataSource ds = createDataSource(getConfigDatabaseCredentials(readConfigJSON()));
 
         try {
             Connection conn = ds.getConnection();
